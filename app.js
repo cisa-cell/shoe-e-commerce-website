003c41b1d3e08504bc8fcf59802f1dc2c2f6e26c
cisa-cell <cinolsamson2006@gmail.com>
2025-06-15 21:29:13 +0530

    closing the payment page.....adding gallery - the cross button is perfectly aligned in the center of its div component - we did so by making sure the font size is smaller than the height   and width of the div component
    adding a cool shadow
    - took inspo from [50th type shadow in this link](https://codepen.io/airen/pen/eYGKEdz)
    - rn we have applied the shadow to a test component with the class name
      pinkBorder

--- app.js
+++ app.js
@@ -194,6 +194,16 @@ element*/
 
 // -------------------------size selection-----------------------------
    
+const payment=document.querySelector(".payment");
+const close=document.querySelector(".close");
+
+productButton.addEventListener("click", () => {
+    payment.style.display = "flex";
+});
+
+close.addEventListener("click", () => {
+    payment.style.display = "none";
+});
 
 
 
